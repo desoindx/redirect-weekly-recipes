4bdea920bfc1beee0b8fa4da1f317551252fc86c
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-01-20 13:56:57 +0100

    permanent redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: ':slug*',
-        destination: 'https://weekly-recipes.vercel.app/:slug*',
-        permanent: true,
-      }
-    ]
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
